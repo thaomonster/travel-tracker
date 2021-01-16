123064f34408bb809161391b7f024e892dbe992b
Thao Ma <MaThao@gmail.com>
2021-01-16 15:55:49 -0700

    Build out properties

--- src/Destination.js
+++ src/Destination.js
@@ -1,6 +1,11 @@
 class Destination {
   constructor(destination) {
-    
+    this.id = destination.id;
+    this.destination = destionation.destination;
+    this.estimatedLodgingCostPerDay = destination.estimatedLodgingCostPerDay;
+    this.estimatedFlightCostPerPerson = destination.estimatedFlightCostPerPerson;
+    this.image = destination.image;
+    this.alt = destination.alt;
   }
 }
 
