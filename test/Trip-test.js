fa79070126eaf60137e62aaa38715d331baa00e8
Thao Ma <MaThao@gmail.com>
2021-01-16 14:43:19 -0700

    Add trip data

---
+++ test/Trip-test.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import Trip from '../src/Trip';
+import sampleTripData from '../test/test-data/trip-data';
+
+describe('Trip class', () => {
+  let trip, trip1, trip2
+
+  beforeEach(() => {
+    
+  })
+})
---
+++ test/test-data/trip-data.js
@@ -0,0 +1,44 @@
+let sampleTrips = {
+  trips: [
+    {
+      "id": 71,
+      "userID": 38,
+      "destinationID": 28,
+      "travelers": 1,
+      "date": "2020/05/26",
+      "duration": 11,
+      "status": "pending",
+      "suggestedActivities": []
+    },
+    {
+      "id": 72,
+      "userID": 38,
+      "destinationID": 25,
+      "travelers": 2,
+      "date": "2019/12/30",
+      "duration": 17,
+      "status": "approved",
+      "suggestedActivities": []
+    },
+    {
+      "id": 94,
+      "userID": 38,
+      "destinationID": 12,
+      "travelers": 3,
+      "date": "2019/11/28",
+      "duration": 13,
+      "status": "approved",
+      "suggestedActivities": []
+    },
+    {
+      "id": 135,
+      "userID": 38,
+      "destinationID": 6,
+      "travelers": 4,
+      "date": "2020/04/02",
+      "duration": 6,
+      "status": "approved",
+      "suggestedActivities": []
+    },
+  ]
+}
