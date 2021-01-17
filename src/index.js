a1646e87ffc6636ee4fd398d910ed3b5d1ba5961
Thao Ma <MaThao@gmail.com>
2021-01-16 23:41:49 -0700

    Add domUpdate file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/index.scss';
 import Traveler from '../src/Traveler';
 import Trip from '../src/Trips';
@@ -9,10 +5,6 @@ import TripsRepo from '../src/TripsRepo';
 import Destination from '../src/Destinations';
 import DestinationsRepo from '../src/DestinationsRepo'
 import {getData} from '../src/apis'
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
-
-console.log('This is the JavaScript entry file - your code begins here.');
+import domUpdates from '../src/domUpdates'
 
 getData('travelers')
