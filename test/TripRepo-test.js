c1d08609cec2e642f66744150c20cb2d2d6c32b6
Thao Ma <MaThao@gmail.com>
2021-01-18 22:43:39 -0700

    Add test to filter pending trips

--- test/TripRepo-test.js
+++ test/TripRepo-test.js
@@ -72,6 +72,33 @@ describe('TripRepo Class', () => {
       ]);
     });
 
+    it('should filter pending trips by year', () => {
+      tripRepo.filterPendingTrips('2019');
+
+      expect(tripRepo.filterPendingTrips('2019')).to.deep.eq([
+        {
+          "id": 2,
+          "userID": 35,
+          "destinationID": 25,
+          "travelers": 5,
+          "date": "2020/10/04",
+          "duration": 18,
+          "status": "pending",
+          "suggestedActivities": []
+        },
+        {
+          "id": 3,
+          "userID": 3,
+          "destinationID": 22,
+          "travelers": 4,
+          "date": "2020/05/22",
+          "duration": 17,
+          "status": "pending",
+          "suggestedActivities": []
+        },
+      ])
+    })
+
     it('should calculate total trip cost', () => {
       const destinations = [
         {
